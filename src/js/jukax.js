e45ebfc5a61992f0de69529184bf51ab407d69be
Moez Bouhlel <bmoez.j@gmail.com>
2013-08-11 13:26:24 +0100

    updates

--- src/js/jukax.js
+++ src/js/jukax.js
@@ -4,13 +4,18 @@ var ERROR_CREATING_USER = 4;
 var ERROR_QUERY_FAILURE = 5;
 var ERROR_LOGIN = 6;
 var ERROR_REFRESHING_DATA = 7;
+var ERROR_DELETING_USER = 8;
+var ERROR_UNVALID_INPUT = 9;
+var ERROR_UPDATING_PASSWORD = 10;
+var ERROR_CLEANINGUP_EVENTS = 11;
 
-Kii.initializeWithSite("ea716d13", "60ac553a1539a79cf9f44a98642be971", KiiSite.US);
+var initialize = function(appID, appKey, kii_site) {
+    kii_site = typeof kii_site !== 'undefined' ? kii_site : KiiSite.US;
+    Kii.initializeWithSite(appID, appKey, kii_site);
+};
 
 var accountCreate = function(username, password, fn) {
-    fn = typeof fn !== 'undefined' ? fn : {
-    }
-    ;
+    fn = typeof fn !== 'undefined' ? fn : {};
     try {
         user = KiiUser.userWithUsername(username, password);
         user.register({
@@ -29,7 +34,7 @@ var accountCreate = function(username, password, fn) {
                             },
                             failure: function(obj, error) {
                                 if ("failure" in fn) {
-                                    fn.failure({type: 7, message: error})
+                                    fn.failure({type: 7, message: error});
                                 }
                                 ;
                             }});
@@ -101,4 +106,97 @@ var accountLogin = function(username, password, fn) {
         }
     });
 
-}
+};
+
+var accountLogout = function() {
+    data.save({
+        success: function() {
+            KiiUser.logOut();
+        },
+        failure: function() {
+            KiiUser.logOut();
+        }});
+};
+
+var accountDelete = function(fn) {
+    fn = typeof fn !== 'undefined' ? fn : {};
+    user.delete({
+        success: function() {
+            if ("success" in fn) {
+                fn.success();
+            }
+        },
+        failure: function(user, errorString) {
+            if ("failure" in fn) {
+                fn.failure({type: 8, message: errorString});
+            }
+        }});
+};
+
+var accountUpdatePassword = function(old_pw, new_pw, fn) {
+    try {
+        if (old_pw != "" && old_pw != null && new_pw != "" && new_pw != null) {
+            user.updatePassword(old_pw, new_pw, {
+                success: function(u) {
+                    user = u;
+                    user.refresh({
+                        success: function(u) {
+                            user = u;
+                            if ("success" in fn) {
+                                fn.success();
+                            }
+                        },
+                        failure: function(u, e) {
+                            if ("failure" in fn) {
+                                fn.failure({type: 7, message: e});
+                            }
+                        }});
+                },
+                failure: function(user, errorString) {
+                    if ("failure" in fn) {
+                        fn.failure({type: 10, message: e});
+                    }
+                }});
+        } else {
+            if ("failure" in fn) {
+                fn.failure({type: 9, message: "Unvalid Input"});
+            }
+        }
+    } catch (e) {
+        throw e.message
+    }
+};
+
+var eventsCleanup = new function(fn) {
+    data.delete({
+        success: function() {
+            data = user.bucketWithName("data").createObject();
+            data.set("data", {});
+            data.save({
+                success: function(o) {
+                    data = o;
+                    data.refresh({
+                        success: function(d) {
+                            data = d
+                            if ("success" in fn) {
+                                fn.success();
+                            }
+                        },
+                        failure: function(obj, errorString) {
+                            if ("failure" in fn) {
+                                fn.failure({type: 7, message: errorString});
+                            }
+                        }});
+                },
+                failure: function(obj, errorString) {
+                    if ("failure" in fn) {
+                        fn.failure({type: 3, message: errorString});
+                    }
+                }});
+        },
+        failure: function(obj, errorString) {
+            if ("failure" in fn) {
+                fn.failure({type: 11, message: errorString});
+            }
+        }});
+};
