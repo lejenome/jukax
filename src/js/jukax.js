69afb7488a1cb14bca45fa4b89c84b905727a8b1
Moez Bouhlel <bmoez.j@gmail.com>
2013-08-11 01:06:12 +0100

    updates

--- src/js/jukax.js
+++ src/js/jukax.js
@@ -1,66 +1,104 @@
 var user, data, bucket;
 var ERROR_SAVING_DATA = 3;
 var ERROR_CREATING_USER = 4;
-var 
+var ERROR_QUERY_FAILURE = 5;
+var ERROR_LOGIN = 6;
+var ERROR_REFRESHING_DATA = 7;
 
 Kii.initializeWithSite("ea716d13", "60ac553a1539a79cf9f44a98642be971", KiiSite.US);
 
-var accountCreate = function(username, password, fn={}){
-    try{
+var accountCreate = function(username, password, fn) {
+    fn = typeof fn !== 'undefined' ? fn : {
+    }
+    ;
+    try {
         user = KiiUser.userWithUsername(username, password);
         user.register({
             success: function(Auser) {
                 bucket = Auser.bucketWithName("data");
-                    data = bucket.createObject();
-                    data.set("data", {});
-                    data.saveAllFields({
-                        success: function(theObject) {
-                            theObject.refresh({ success:function(){
+                data = bucket.createObject();
+                data.set("data", {});
+                data.saveAllFields({
+                    success: function(theObject) {
+                        theObject.refresh({
+                            success: function(obj) {
                                 data = obj;
-                                if( "success" in fn ) {fn.success();}
+                                if ("success" in fn) {
+                                    fn.success();
+                                }
+                            },
+                            failure: function(obj, error) {
+                                if ("failure" in fn) {
+                                    fn.failure({type: 7, message: error})
+                                }
+                                ;
                             }});
-                        },
-                            failure: function(theObject, errorString) {
-                                if ( "failure" in fn) {fn.failure( {type: 3, message: errorString} );}
-                            }
-                        });
                     },
-                    failure: function(theUser, errorString) {
-                        if ( "failure" in fn) { fn.failure( {type: 4, message: errorString} );}
+                    failure: function(theObject, errorString) {
+                        if ("failure" in fn) {
+                            fn.failure({type: 3, message: errorString});
+                        }
                     }
                 });
+            },
+            failure: function(theUser, errorString) {
+                if ("failure" in fn) {
+                    fn.failure({type: 4, message: errorString});
+                }
+            }
+        });
     } catch (e) {
         throw e.message;
     }
 };
 
-var accountLogin = function(username, password, fn={}){
+var accountLogin = function(username, password, fn) {
+    fn = typeof fn !== 'undefined' ? fn : {};
     KiiUser.authenticate(username, password, {
-            success: function(Auser) {
-                _.user = Auser;
-                _.bucket = _.user.bucketWithName("data");
-                var query = KiiQuery.queryWithClause();
-                var queryCallbacks = {
-                    success: function(queryPerformed, r, nextQuery) {
-                        r[0].refresh({success: function(obj) {
-                                _.data = obj;
-                                if (_.data.get("data") == undefined) {
-                                    _.data.set("data", {});
-                                }
-                                _.data.save({ success:_.login_next(obj) });
-                            }});
-                    },
-                    failure: function(queryPerformed, anErrorString) {
-                        console.log("query failure");
+        success: function(Auser) {
+            user = Auser;
+            bucket = user.bucketWithName("data");
+            var query = KiiQuery.queryWithClause();
+            var queryCallbacks = {
+                success: function(queryPerformed, r, nextQuery) {
+                    r[0].refresh({
+                        success: function(obj) {
+                            data = obj;
+                            if (data.get("data") == undefined) {
+                                data.set("data", {});
+                            }
+                            data.save({
+                                success: function(obj) {
+                                    data = obj;
+                                    if ("success" in fn) {
+                                        fn.success();
+                                    }
+                                },
+                                failure: function(obj, errorString) {
+                                    if ("failure" in fn) {
+                                        fn.failure({type: 3, message: errorString});
+                                    }
+                                }});
+                        },
+                        failure: function(obj, errorString) {
+                            if ("failure" in fn) {
+                                fn.failure({type: 7, message: errorString});
+                            }
+                        }});
+                },
+                failure: function(queryPerformed, errorString) {
+                    if ("failure" in fn) {
+                        fn.failure({type: 5, message: errorString});
                     }
-                };
-                _.bucket.executeQuery(query, queryCallbacks);
-            },
-            failure: function(theUser, anErrorString) {
-                $.mobile.hidePageLoadingMsg();
-                alert("Unable to register: " + anErrorString);
-                Kii.logger("Unable to register user: " + anErrorString);
+                }
+            };
+            bucket.executeQuery(query, queryCallbacks);
+        },
+        failure: function(theUser, errorString) {
+            if ("failure" in fn) {
+                fn.failure({type: 6, message: errorString});
             }
-        });
+        }
+    });
 
 }
